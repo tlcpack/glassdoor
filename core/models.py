83ce2e5e23008c21730dac9c1ed804abf1b69abc
Taylor Cooke <taylorcooke@gmail.com>
2020-09-28 11:21:32 -0400

    getting started with models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Company(models.Model):
+  name = 
+  address = 
+  website = 
+
+  def __str__(self):
+      return self.name
+  
+
+
+class Review(models.Model):
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    review = models.TextField()
+    rating = models.IntegerField(validators=[
+        MaxValueValidator(5),
+        MinValueValidator(1)
+    ])
