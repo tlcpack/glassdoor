2030253ff331f696c7881986bd8e346f3ac4a8b6
Taylor Cooke <taylorcooke@gmail.com>
2020-09-28 14:50:27 -0400

    company showing on page

--- core/views.py
+++ core/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Company, Review
 
 # Create your views here.
 def index(request):
-  return render(request, 'index.html')
+  companies = Company.objects.all()
+  return render(request, 'index.html', { 'companies': companies})
