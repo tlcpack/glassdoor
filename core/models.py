1476ad06bc8f1d7cf59be5811c56bc850df8cff9
Taylor Cooke <taylorcooke@gmail.com>
2020-09-28 21:36:33 -0400

    update content field. reviews displaying

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.contrib.auth.models import User
 
@@ -19,7 +20,7 @@ class Company(models.Model):
 class Review(models.Model):
     company = models.ForeignKey(Company, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    review = models.TextField()
+    content = models.TextField()
     rating = models.IntegerField(validators=[
         MaxValueValidator(5),
         MinValueValidator(1)
