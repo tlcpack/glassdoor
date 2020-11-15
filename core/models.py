876db6639dda9902e01dadb276b43e6fe57c22ff
Taylor Cooke <taylorcooke@gmail.com>
2020-11-14 21:42:39 -0500

    adding job model

--- core/models.py
+++ core/models.py
@@ -20,15 +20,26 @@ class Company(models.Model):
 
 
 class Review(models.Model):
-    company = models.ForeignKey('Company', related_name='reviews', on_delete=models.CASCADE)
-    content = models.TextField()
-    rating = models.IntegerField(validators=[
-        MaxValueValidator(5),
-        MinValueValidator(1)
-    ])
+  company = models.ForeignKey('Company', related_name='reviews', on_delete=models.CASCADE)
+  content = models.TextField()
+  rating = models.IntegerField(validators=[
+      MaxValueValidator(5),
+      MinValueValidator(1)
+  ])
 
-    def get_absolute_url(self):
-      return reverse('reviews', args=[str(self.id)])
+  def get_absolute_url(self):
+    return reverse('reviews', args=[str(self.id)])
 
-    def __str__(self):
-        return f"{self.company!r} - {self.rating!r}"
+  def __str__(self):
+      return f"{self.company!r} - {self.rating!r}"
+
+class Job(models.Model):
+  company = models.ForeignKey('Company', related_name='jobs', on_delete=models.CASCADE)
+  description = models.TextField(max_length=1000)
+  requirements = models.TextField(max_length=1000)
+  applicant_name = models.TextField(max_length=100)
+  applicant_email = models.TextField(max_length=50)
+  resume = models.FileField()
+
+  def __str__(self):
+    return self.name
